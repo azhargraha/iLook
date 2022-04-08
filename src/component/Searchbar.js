81e64f529cb77fa65bcb42c767a885ba10564eb8
Azhar Nugraha <57381268+azhargraha@users.noreply.github.com>
2022-04-08 23:20:31 +0700

    add animation

--- src/component/Searchbar.js
+++ src/component/Searchbar.js
@@ -1,13 +1,30 @@
 import React from 'react';
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import gsap from 'gsap';
 
 import '../style/Input.scss';
 
-const Searchbar = ({ placeholder }) => {
+const expand = () => {
+  return {
+    opacity: 0,
+    ease: 'Power3.easeOut',
+    width: 0,
+    marginLeft: '50%',
+    delay: .6,
+    duration: 1.2
+  };
+};
+
+const Searchbar = ({ placeholder = '', withAnimation = false, value = '', getInput = null, onSubmit }) => {
+  const searchbar = useRef(null);
   const [isType, setIsType] = useState(false);
+  const [valueState, setValueState] = useState(value);
   const input = useRef(null);
 
   const onChangeInput = (e) => {
+    setValueState(e.target.value);
+    getInput(e.target.value);
+
     if (e.target.value !== '') {
       setIsType(true);
     } else {
@@ -15,25 +32,29 @@ const Searchbar = ({ placeholder }) => {
     }
   };
 
+  useEffect(() => {
+    if (withAnimation) gsap.from(searchbar.current, expand());
+  }, []);
+
   const clearInput = () => {
-    input.current.value = '';
+    setValueState('');
     setIsType(false);
   };
 
   return (
-    <div className="searchbar-container">
+    <form ref={searchbar} onSubmit={onSubmit} className="searchbar-container">
         <div className="icon">
           <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M11 18C14.866 18 18 14.866 18 11C18 7.13401 14.866 4 11 4C7.13401 4 4 7.13401 4 11C4 14.866 7.13401 18 11 18Z" strokeWidth="2" strokeLinecap="round"/>
             <path d="M21 21L16 16" strokeWidth="2" strokeLinecap="round"/>
           </svg>
         </div>
-        <input ref={input} type="text" placeholder={placeholder} onChange={onChangeInput}/>
+        <input ref={input} type="text" value={valueState ? valueState : ''} placeholder={placeholder} onChange={onChangeInput} />
         <div className="buttons-container">
-          {isType && <button className="clear-btn" onClick={clearInput}>clear</button>}
+          {isType && <button type="button" className="clear-btn" onClick={clearInput}>clear</button>}
           <button type="submit" className="submit-btn">Search</button>
         </div>
-    </div>
+    </form>
   )
 }
 
