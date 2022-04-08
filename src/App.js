042cc110ab926d1767d4c9ea35400274ddff88df
Azhar Nugraha <57381268+azhargraha@users.noreply.github.com>
2022-04-08 23:12:55 +0700

    update routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import axios from 'axios';
+import gsap from 'gsap';
 
 import Navbar from './component/Navbar';
 
@@ -10,12 +11,23 @@ import PrivacyPolicy from './page/PrivacyPolicy';
 import About from './page/About';
 import Register from './page/Register';
 import SignIn from './page/SignIn';
-import Dashboard from './page/Dashboard';
+import Plan from './page/Plan';
+import Package from './page/Package';
+import Discover from './page/Discover';
+import Place from './page/Place';
+import MyPackage from './page/MyPackage';
+import CreatePlace from './page/CreatePlace';
 
 axios.defaults.baseURL = 'http://localhost:8000/';
 axios.defaults.withCredentials = true;
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 axios.defaults.headers.post['Accept'] = 'application/json';
+
+gsap.config({
+  force3D: false,
+  nullTargetWarn: false,
+  trialWarn: false,
+});
 
 export default function App() {
   return (
@@ -23,16 +35,26 @@ export default function App() {
       <div className="App">
         <Navbar />
         <Routes>
-          {/* landing page */}
           <Route path='/' element={<Explore />} />
           <Route path='/features' element={<Features />} />
           <Route path='/privacy-policy' element={<PrivacyPolicy />} />
           <Route path='/about' element={<About />} />
           <Route path='/register' element={<Register />} />
           <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/discover' element={<Discover />} />
 
-          {/* dashboard - user */}
-          <Route path='/dashboard' element={<Dashboard />} />
+          {/* user */}
+          <Route path='/plans' element={<Plan />} />
+          <Route path='/packages' element={<Package />} />
+
+          {/* tourguide */}
+          <Route path='/my-packages' element={<MyPackage />} />
+
+          {/* admin */}
+          <Route path='/place' element={<Place />} />
+          <Route path='/place/create' element={<CreatePlace />} />
+
+          <Route path='*' element={<Navigate to='/' replace />} />
         </Routes>
       </div>
     </Router>
