63e6338192f9132eb2fac220c61750efce5143af
Azhar Nugraha <57381268+azhargraha@users.noreply.github.com>
2022-04-08 23:24:05 +0700

    add animation & fix axios handle response

--- src/page/Register.js
+++ src/page/Register.js
@@ -1,15 +1,47 @@
 import React from 'react';
 import { useState, useEffect, useRef } from 'react';
+import gsap from 'gsap';
 import axios from 'axios';
 
 import Textfield from '../component/Textfield';
 import Button from '../component/Button';
 
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+
+const fadeIn = ({y = 20, duration = 1, stagger = .2, staggerDirection = 1, delay = .2}) => {
+  return {
+    opacity: 0,
+    ease: 'Power3.easeOut',
+    delay: delay,
+    duration: duration,
+    stagger: {
+        y: y,
+        each: stagger * staggerDirection
+    }
+  };
+};
+
+const expand = ({ delay = 0 }) => {
+  return {
+    opacity: 0,
+    ease: 'Power3.easeOut',
+    scaleY: 0,
+    duration: .6,
+    delay: delay,
+    stagger: {
+      each: .2
+    }
+  };
+};
 
 const Register = () => {
+  const navigate = useNavigate();
+
   const sectionOne = useRef(null);
   const sectionTwo = useRef(null);
+  const heading = useRef(null);
+  const buttons = useRef(null);
+  const footer = useRef(null);
 
   const [role, setRole] = useState('user');
   const [accountInfo, setAccountInfo] = useState(null);
@@ -22,7 +54,7 @@ const Register = () => {
   const [location, setLocation] = useState(null);
   const onChangeMethods = [
     setName, setUsername, setPhoneNumber, setPassword, setLocation
-  ]
+  ];
 
   const pickRole = (e) => {
     setRole(e.target.value);
@@ -38,10 +70,31 @@ const Register = () => {
 
     axios.get('sanctum/csrf-cookie').then(response => {
       axios.post('api/register', accountInfo)
-          .then(res => console.log(res))
+          .then(res => {
+            console.log(res)
+            if (res.data.status === 200) {
+              navigate('/sign-in', { state: username });
+            } else {
+              
+            }
+          })
           .catch(err => console.log(err));
     });
   }
+
+  useEffect(() => {
+    gsap.from(heading.current.childNodes, fadeIn({}));
+  }, []);
+
+  useEffect(() => {
+    if (currentSection === 'one') {
+      gsap.from(sectionOne.current.childNodes[0], fadeIn({ delay: .2 }));
+      gsap.from(sectionOne.current.childNodes[1].children, expand({ delay: .4 }));      
+    } else if (currentSection === 'two') {
+      gsap.from(sectionTwo.current.childNodes[0], fadeIn({ delay: .6 }));
+      gsap.from(footer.current, fadeIn({ y: -15, delay: .3 }));
+    }
+  }, [currentSection]);
 
   useEffect(() => {
     if (role === 'user') {
@@ -71,47 +124,65 @@ const Register = () => {
   return (
     <div className="register-container">
         <div className="form-wrapper">
-          <div className="heading-wrapper">
+          <div ref={heading} className="heading-wrapper">
             <h1>Getting started with us</h1>
             <p>Please fill out the form below first, then you're good to go. Already have an account? <Link to="/sign-in">Sign in</Link>.</p>
           </div>
             <form onSubmit={handleSubmit}>
               <div className="row">
-                <div ref={sectionOne} className={currentSection === 'one'? 'section active' : 'section'}>
-                  <h3>Pick your role</h3>
-                  <div className="selection-container">
-                    <div className="selection-item">
-                      <input type="radio" name="role" id="user" value="user" onClick={e => pickRole(e)} defaultChecked={role === 'user'} />
-                      <label htmlFor="user">User</label>
+                {
+                  currentSection === 'one' && (
+                    <div ref={sectionOne} className="section active">
+                      <h3>Pick your role</h3>
+                      <div className="selection-container">
+                        <div className="selection-item">
+                          <input type="radio" name="role" id="user" value="user" onClick={e => pickRole(e)} defaultChecked={role === 'user'} />
+                          <label htmlFor="user">User</label>
+                        </div>
+                        <div className="selection-item">
+                          <input type="radio" name="role" id="tourguide" value="tourguide" onClick={e => pickRole(e)} defaultChecked={role === 'tourguide'} />
+                          <label htmlFor="tourguide">Tour guide</label>
+                        </div>
+                      </div>
                     </div>
-                    <div className="selection-item">
-                      <input type="radio" name="role" id="tourguide" value="tourguide" onClick={e => pickRole(e)} defaultChecked={role === 'tourguide'} />
-                      <label htmlFor="tourguide">Tour guide</label>
+                  )
+                }
+                {
+                  currentSection === 'two' && (
+                    <div ref={sectionTwo} className="section active">
+                      <h3>Set up your account</h3>
+                      <div className="fields-container">
+                        <FormByRole role={role} onChangeMethods={onChangeMethods} />
+                      </div>
                     </div>
-                  </div>
-                </div>
-                <div ref={sectionTwo} className={currentSection === 'two'? 'section active' : 'section'}>
-                  <h3>Set up your account</h3>
-                  <div className="fields-container">
-                    <FormByRole role={role} onChangeMethods={onChangeMethods} />
-                  </div>
-                </div>
+                  )
+                }
               </div>
                 <div className="submit-section-wrapper">
-                  <div className="buttons-wrapper">
-                    <Button
-                    label={currentSection === 'one' ? 'Next' : 'Back'} 
-                    onClick={e => sectionControl(e)}
-                    style={currentSection === 'one' ? 'primary' : 'label-only'} 
-                    />
+                  <div ref={buttons} className="buttons-wrapper">
                     {
-                      currentSection === 'two' && 
-                      (
+                      currentSection === 'one' && (
+                        <Button
+                        label="Next"
+                        onClick={e => sectionControl(e)}
+                        style="primary"
+                        />
+                      )
+                    }
+                    {
+                      currentSection === 'two' && (
+                        <>
+                          <Button
+                          label="Back" 
+                          onClick={e => sectionControl(e)}
+                          style="label-only"
+                          />
                           <Button type="submit" label="Register" />
-                        )
+                        </>
+                      )
                     }
                   </div>
-                  {currentSection === 'two' && <p>By clicking Register, I have read and understood the <Link to="/privacy-policy">Privacy policy</Link>.</p>}
+                  {currentSection === 'two' && <p ref={footer}>By clicking Register, I have read and understood the <Link to="/privacy-policy" target="_blank">Privacy policy</Link>.</p>}
                 </div>
             </form>
         </div>
@@ -131,20 +202,20 @@ const FormByRole = ({ role, onChangeMethods }) => {
     if (role === 'user') {
       setForm(
         <>
-          <Textfield type="text" label="Name" placeholder="Enter your name" required={true} onChange={setName} />
-          <Textfield type="text" label="Username" placeholder="Create your username" required={true} onChange={setUsername} />
-          <Textfield type="number" label="Phone number" placeholder="Enter your phone" required={true} onChange={setPhoneNumber} />
-          <Textfield type="password" label="Password" placeholder="Contain at least 8 characters" required={true} onChange={setPassword} />
+          <Textfield type="text" label="Name" placeholder="Enter your name" required={true} getInput={setName} />
+          <Textfield type="text" label="Username" placeholder="Create your username" required={true} getInput={setUsername} />
+          <Textfield type="number" label="Phone number" placeholder="Enter your phone" required={true} getInput={setPhoneNumber} />
+          <Textfield type="password" label="Password" placeholder="Contain at least 8 characters" required={true} getInput={setPassword} />
         </>
       );
     } else if (role === 'tourguide') {
       setForm(
         <>
-          <Textfield type="text" label="Agency name" placeholder="Enter your name" required={true} onChange={setName} />
-          <Textfield type="text" label="Username" placeholder="Enter your name" required={true} onChange={setUsername} />
-          <Textfield type="number" label="Phone number" placeholder="Enter your phone" required={true} onChange={setPhoneNumber} />
-          <Textfield type="password" label="Password" placeholder="Contain at least 8 characters" required={true} onChange={setPassword} />
-          <Textfield type="text" label="Location" placeholder="Enter your location" required={true} onChange={setLocation} />
+          <Textfield type="text" label="Agency name" placeholder="Enter your name" required={true} getInput={setName} />
+          <Textfield type="text" label="Username" placeholder="Enter your name" required={true} getInput={setUsername} />
+          <Textfield type="number" label="Phone number" placeholder="Enter your phone" required={true} getInput={setPhoneNumber} />
+          <Textfield type="password" label="Password" placeholder="Contain at least 8 characters" required={true} getInput={setPassword} />
+          <Textfield type="text" label="Location" placeholder="Enter your location" required={true} getInput={setLocation} />
         </>
       );
     }
--- src/page/SignIn.js
+++ src/page/SignIn.js
@@ -1,21 +1,61 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import gsap from 'gsap';
 
 import Textfield from '../component/Textfield';
 import Button from '../component/Button';
 
-import { Link } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
+import axios from 'axios';
+
+
+const fadeIn = ({y = 20, duration = 1, stagger = .2, staggerDirection = 1, delay = .2}) => {
+  return {
+    opacity: 0,
+    ease: 'Power3.easeOut',
+    delay: delay,
+    duration: duration,
+    stagger: {
+        y: y,
+        each: stagger * staggerDirection
+    }
+  };
+};
 
 const SignIn = () => {
-  const [authInfo, setAuthInfo] = useState(null);
+  const { state } = useLocation();
 
-  const [username, setUsername] = useState(null);
+  const heading = useRef(null);
+  const footer = useRef(null);
+
+  const navigate = useNavigate();
+
+  const [authInfo, setAuthInfo] = useState(null);
+  const [authFail, setAuthFail] = useState(false);
+
+  const [username, setUsername] = useState(state ? state : null);
   const [password, setPassword] = useState(null);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(authInfo);
+
+    axios.get('sanctum/csrf-cookie').then(response => {
+      axios.post('api/login', authInfo)
+          .then(res => {
+            if (res.data.status === 200) {
+              navigate('/');
+            } else {
+              setAuthFail(true);
+            }
+          })
+          .catch(err => console.log(err));
+    });
   }
+
+  useEffect(() => {
+    gsap.from(heading.current.childNodes, fadeIn({}));
+    gsap.from(footer.current, fadeIn({ y: -15, delay: .6 }));
+  }, []);
 
   useEffect(() => {
     setAuthInfo({
@@ -27,7 +67,7 @@ const SignIn = () => {
   return (
     <div className="sign-in-container">
         <div className="form-wrapper">
-          <div className="heading-container">
+          <div ref={heading} className="heading-container">
             <h1>Hey there!</h1>
             <p>Glad to have you back. Thank you for choosing our app.</p>
           </div>
@@ -35,14 +75,15 @@ const SignIn = () => {
           <form onSubmit={handleSubmit}>
               <div className="section">
                 <div className="fields-container">
-                  <Textfield type="text" label="Username" placeholder="Enter your username" required={true} onChange={setUsername} />
-                  <Textfield type="password" label="Password" placeholder="Enter password" required={true} onChange={setPassword} />
+                  <Textfield type="text" label="Username" placeholder="Enter your username" required={true} getInput={setUsername} valueProp={state} />
+                  <Textfield type="password" label="Password" placeholder="Enter password" required={true} getInput={setPassword} />
+                  {authFail && <p className="error">Incorrect username or password. Please try again. </p>}
                 </div>
               </div>
 
               <div className="submit-section-wrapper">
                 <Button type="submit" label="Sign in" />
-                <p>Don't have an account yet? <Link to="/register">Get started</Link>.</p>
+                <p ref={footer} >Don't have an account yet? <Link to="/register">Get started</Link>.</p>
               </div>
           </form>
         </div>
