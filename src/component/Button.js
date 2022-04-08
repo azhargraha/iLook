776fa957ed753a72695c901e2a9ff9feef74a250
Azhar Nugraha <57381268+azhargraha@users.noreply.github.com>
2022-04-08 23:19:05 +0700

    update gsap property

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
+import gsap from 'gsap';
 
 import '../style/Button.scss';
 
+const fadeIn = () => {
+  return {
+    y: -4,
+    opacity: 0,
+    ease: 'Power3.easeOut',
+    delay: .6,
+    duration: .4
+  };
+};
+
+const expand = () => {
+  return {
+    opacity: 0,
+    ease: 'Power3.easeOut',
+    delay: .4,
+    height: 0,
+    duration: .6
+  };
+};
+
 const Button = ({ type='button', label='', onClick=null, style='primary' }) => {
+  const container = useRef(null);
+  const labelRef = useRef(null);
+
+  useEffect(() => {
+    gsap.from(container.current, expand());
+    gsap.from(labelRef.current, fadeIn());
+  }, []);
+  
+
   return (
-    <button type={type} className={`button-container ${style !== 'primary' ? style : 'primary'}`} onClick={onClick}>
-        <h4>{label}</h4>
+    <button ref={container} type={type} className={`button-container ${style !== 'primary' ? style : 'primary'}`} onClick={onClick}>
+        <h4 ref={labelRef}>{label}</h4>
     </button>
   )
 };
