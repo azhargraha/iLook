43281755ef62d3769eff2e60af4bbc69ed1034fa
Azhar Nugraha <57381268+azhargraha@users.noreply.github.com>
2022-04-12 03:38:24 +0700

    add drag drop file functionality

--- src/page/CreatePlace.js
+++ src/page/CreatePlace.js
@@ -1,13 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Textfield from '../component/Textfield';
 import Button from '../component/Button';
 
 import '../style/CreatePlace.scss';
 
 const CreatePlace = () => {
+  const [isDragOver, setIsDragOver] = useState(false);
+
+  const [imgSrc, setImgSrc] = useState(null);
+  const [placeName, setPlaceName] = useState(null);
+  const [category, setCategory] = useState(null);
+  const [price, setPrice] = useState(null);
+  const [description, setDescription] = useState(null);
+  const [location, setLocation] = useState(null);
+  const [placeData, setPlaceData] = useState(null);
+  const validFileType = ['image/jpeg', 'image/jpg', 'image/png'];
+
   const handleSubmit = (e) => {
     return;
   }
+
+  const dragOver = (e) => {
+    e.preventDefault();
+    setIsDragOver(true);
+  }
+  
+  const dragLeave = (e) => {
+    e.preventDefault();
+    setIsDragOver(false);
+  }
+  
+  const dropFile = (e) => {
+    e.preventDefault();
+    let file = e.dataTransfer.files[0];
+    if (validFileType.includes(file.type)) {
+      let fileReader = new FileReader();
+      fileReader.onload = () => {
+        let fileURL = fileReader.result;
+        setImgSrc(fileURL);
+        setIsDragOver(false);
+      }
+      fileReader.readAsDataURL(file);
+    } else {
+      console.log('false');
+    }
+  }
+
+  const computerFileSelect = (e) => {
+    e.preventDefault();
+    let file = e.target.files[0];
+    if (validFileType.includes(file.type)) {
+      let fileReader = new FileReader();
+      fileReader.onload = () => {
+        let fileURL = fileReader.result;
+        setImgSrc(fileURL);
+      }
+      fileReader.readAsDataURL(file);
+    } else {
+      console.log('false')
+    }
+  }
+
+  const clearFile = () => {
+    setImgSrc(null);
+  }
+
+  useEffect(() => {
+    setPlaceData({
+      placeName,
+      category,
+      price,
+      description,
+      location
+    });
+  }, [placeName, category, price, description, location]);
 
   return (
     <div className="create-place-container">
@@ -19,12 +85,43 @@ const CreatePlace = () => {
           <form onSubmit={handleSubmit}>
               <div className="section">
                 <div className="fields-container">
-                  <Textfield type='text' label="Place name" placeholder='Enter place name' />
-                  <Textfield type='text' label="Category" placeholder='Enter place category' />
-                  <Textfield type='number' label="Price" placeholder='Enter ticket price' />
-                  <Textfield type='text' label="Place name" placeholder='Enter place name' />
-                  <Textfield type='text' label="Description" placeholder='Enter place description' />
-                  <Textfield type='text' label="Location" placeholder='Enter place location' />
+                  <div id="drop-area-wrapper">
+                    <label>Thumbnail image</label>
+                    <div id="drop-area"
+                      className={isDragOver && 'dragover'}
+                      onDragLeave={dragLeave}
+                      onDragOver={dragOver}
+                      onDrop={dropFile}
+                    >
+                      {
+                        imgSrc ? 
+                        (<>
+                          <img src={imgSrc} className="image-preview" />
+                          <div id="button-wrapper">
+                            <button type="button" id="delete-btn" onClick={clearFile}>
+                                <svg width="100%" height="100%" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M19 19L5 5" strokeWidth="5" strokeLinecap="round"/>
+                                <path d="M5 19L19 5" strokeWidth="5" strokeLinecap="round"/>
+                                </svg>
+                            </button>
+                          </div>
+                        </>)
+                        : 
+                        (<>
+                          <p>Drop your image file here</p>
+                          <p>or</p>
+                          <label htmlFor="image">Browse from your computer</label>
+                          <input type="file" name="image" id="image" accept=".jpeg, .jpg, .png" onChange={computerFileSelect} required />
+                        </>)
+                      }
+
+                    </div>
+                  </div>
+                  <Textfield type='text' label="Place name" placeholder='Enter place name' getInput={setPlaceName} />
+                  <Textfield type='text' label="Category" placeholder='Enter place category' getInput={setCategory} />
+                  <Textfield type='number' label="Price" placeholder='Enter ticket price' getInput={setPrice} />
+                  <Textfield type='text' label="Description" placeholder='Enter place description' getInput={setDescription} />
+                  <Textfield type='text' label="Location" placeholder='Enter place location' getInput={setLocation} />
                 </div>
               </div>
 
